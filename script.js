18fc78d15f69a9d129682ee95c8a5ab6a8d38b69
nnh6 <56899420+nnh6@users.noreply.github.com>
2024-03-08 01:24:09 -0500

    Update script.js
    Adding myFunction

--- script.js
+++ script.js
@@ -27,3 +27,12 @@
   });
   
 })(jQuery);
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
